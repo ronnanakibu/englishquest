dd31e94cdaa7fddfdb66eb47425348d38e75a8e9
Rony Imanuel Sihombing <ronysihombing07@gmail.com>
2026-05-12 15:01:40 +0700

    Refactor prisma plugin to use FastifyInstance

--- apps/api/src/plugins/prisma.plugin.ts
+++ apps/api/src/plugins/prisma.plugin.ts
@@ -1,5 +1,6 @@
 import fp from 'fastify-plugin'
 import { FastifyPluginAsync } from 'fastify'
+import { FastifyInstance } from 'fastify'
 import { PrismaClient } from '@prisma/client'
 
 declare module 'fastify' {
@@ -17,6 +18,18 @@ const prismaPlugin: FastifyPluginAsync = fp(async (server) => {
   server.decorate('prisma', prisma)
 
   server.addHook('onClose', async (server) => {
+    await server.prisma.$disconnect()
+  })
+})
+
+export default fp(async (server: FastifyInstance, opts) => {
+  const prisma = new PrismaClient()
+  
+  await prisma.$connect()
+  
+  server.decorate('prisma', prisma)
+  
+  server.addHook('onClose', async (server: FastifyInstance) => {
     await server.prisma.$disconnect()
   })
 })
